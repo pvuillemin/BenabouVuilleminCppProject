6967ef5ded20b3b657a53a5ffd79eedd0c0ff978
pvuillemin <vuillemin_pierre@yahoo.fr>
2014-02-21 17:01:13 +0100

    commenting the code

--- MultiArmedBanditBV/MultiArmedBanditBV/main.cpp
+++ MultiArmedBanditBV/MultiArmedBanditBV/main.cpp
@@ -19,17 +19,17 @@ using namespace std;
 int main(int argc, char* args[])
 {
 	
-	Gui gui;
-	gui.Welcome();
-	gui.ChooseArmNumber();
-	gui.SetArms();
-	gui.SetSimulations();
-	mt19937_64 generator;
-	Bandit Bandit (gui.GetArmNumber(),gui.GetChoices(),gui.GetParameters());	
-	Player player;
+	Gui gui;//Initialize the gui
+	gui.Welcome();//welcome window
+	gui.ChooseArmNumber();//choose the number of arm for your bandit
+	gui.SetArms();//Choose the parameters and the type of the arms
+	gui.SetSimulations();//Set the simultation by indicating how many try you want
+	mt19937_64 generator;//Initialization of the PRNG, we use the famous mersenne twister
+	Bandit Bandit (gui.GetArmNumber(),gui.GetChoices(),gui.GetParameters());//Initialize the bandit using the information collected by the gui
+	Player player;//Initialize the player
 
 
-	player.PursuitAlgorithmStrategy(&Bandit, 100, generator,0.1,5);
+	player.PursuitAlgorithmStrategy(&Bandit, gui.GetSimulationNumber(), generator,0.1,gui.GetInitNumber());//play using the parameters collected by the gui
 
 
 	player.WriteDataInCSV("Data/Data.csv");
